4d62b9e8d4e21662f8a697d6918bf0e2d47fd009
= <isalasg_tec@hotmail.com>
2018-03-24 17:07:35 -0600

    Versión inicial

---
+++ CalculosConversiones/CalculosConversiones/InterfazVehiculo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculosConversiones
+{
+    class InterfazVehiculo
+    {
+        private float Velocidad;
+        private float Tiempo;
+        private float Distancia;
+
+        public InterfazVehiculo (){
+            Velocidad = 0;
+            Tiempo = 0;
+            Distancia = 0;
+        }
+
+        public void PedirVelocidad()
+        {
+            Console.Write("Dame velocidad: ");
+            Velocidad = float.Parse(Console.ReadLine());
+        }
+
+        public void PedirTiempo()
+        {
+            Console.Write("Dame tiempo: ");
+            Tiempo = float.Parse(Console.ReadLine());
+        }
+
+        public void PedirDistancia()
+        {
+            Console.Write("Dame distancia: ");
+            Distancia = float.Parse(Console.ReadLine());
+        }
+
+        public float GetVelocidad()
+        {
+            return Velocidad;
+        }
+
+        public float GetTiempo()
+        {
+            return Tiempo;
+        }
+
+        public float GetDistancia()
+        {
+            return Distancia;
+        }
+
+        public void ImprimirVelocidad(float Velocidad)
+        {
+            Console.WriteLine("La velocidad es: " + Velocidad);
+        }
+
+        public void ImprimirTiempo(float Tiempo)
+        {
+            Console.WriteLine("El tiempo es: " + Tiempo);
+        }
+
+        public void ImprimirDistancia(float Distancia)
+        {
+            Console.WriteLine("La distancia es: " + Distancia);
+        }
+
+    }
+}
---
+++ CalculosConversiones/CalculosConversiones/Vehiculo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculosConversiones
+{
+    class Vehiculo
+    {
+        private float Velocidad;
+        private float Tiempo;
+        private float Distancia;
+
+        public Vehiculo()
+        {
+            Velocidad = 0;
+            Tiempo = 0;
+            Distancia = 0;
+        }
+
+        public void SetVelocidad(float Velocidad)
+        {
+            this.Velocidad = Velocidad;
+        }
+
+        public void SetTiempo(float Tiempo)
+        {
+            this.Tiempo = Tiempo;
+        }
+
+        public void SetDistancia(float Distancia)
+        {
+            this.Distancia = Distancia;
+        }
+
+        public float GetVelocidad()
+        {
+            return Velocidad;
+        }
+
+        public float GetTiempo()
+        {
+            return Tiempo;
+        }
+
+        public float GetDistancia()
+        {
+            return Distancia;
+        }
+
+        public void CalcularVelocidad()
+        {
+            Velocidad = Distancia / Tiempo;
+        }
+
+        public void CalcularTiempo()
+        {
+            Tiempo = Distancia / Velocidad;
+        }
+
+        public void CalcularDistancia()
+        {
+            Distancia = Velocidad * Tiempo;
+        }
+    }
+}
