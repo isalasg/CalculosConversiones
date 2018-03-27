0182370e08b27ebb01a45241c233ca4446f05401
Luis Moises Vega Aguero <luis_moises1997@hotmail.com>
2018-03-27 13:46:48 -0600

    Se añadio la clase Moneda y la clase InterfazMoneda

---
+++ CalculosConversiones/CalculosConversiones/InterfazMoneda.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculosConversiones
+{
+    class InterfazMoneda
+    {
+        private double Monto;
+        private double Total;
+
+        public InterfazMoneda()
+        {
+            Monto = 0;
+            Total = 0;
+        }
+
+        public void PedirMonto()
+        { 
+            Console.Write("Escribe la cantidad a cambiar: ");
+            Monto = double.Parse(Console.ReadLine());
+        }
+
+        public double GetMonto()
+        {
+            return Monto;
+        }
+        public void SetMonto(double Monto)
+        {
+            this.Monto = Monto;
+        }
+        public double GetTotal()
+        {
+            return Total;
+        }
+        public void SetTotal(double Total)
+        {
+            this.Total = Total;
+        }
+
+        public void ImprimirTotal(double Total)
+        {
+            Console.WriteLine("Tu tienes : " + Total + " despues de cambiarlo ");
+        }
+    }
+}
---
+++ CalculosConversiones/CalculosConversiones/Moneda.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculosConversiones
+{
+    class Moneda
+    {
+        private double Monto;
+        private double Total;
+
+        public Moneda()
+        {
+            Monto = 0;
+            Total = 0;
+        }
+
+        public double GetMonto()
+        {
+            return Monto;
+        }
+        public void SetMonto(double Monto)
+        {
+            this.Monto = Monto;
+        }
+        public double GetTotal()
+        {
+            return Total;
+        }
+        public void SetTotal(double Total)
+        {
+            this.Total = Total;
+        }
+        //Pesos a Dolares
+        public void CambiarPesoDolar()
+        {
+            Total = Monto * 0.05455;
+        }
+        //Pesos a Euros
+        public void CambiarPesoEuro()
+        {
+            Total = Monto * 0.04383;
+        }
+
+        //Dolares a Pesos
+        public void CambiarDolarPeso()
+        {
+            Total = Monto * 18.3345;
+        }
+        //Dolares a Euros
+        public void CambiarDolarEuro()
+        {
+            Total = Monto * 0.80335;
+        }
+
+        //Euros a Pesos
+        public void CambiarEuroPeso()
+        {
+            Total = Monto * 22.8134;
+        }
+        //Euros a Dolares
+        public void CambiarEuroDolar()
+        {
+            Total = Monto * 1.2443;
+        }
+
+    }
+}
