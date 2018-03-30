f70ff1aa38678285ec2795b31c19bbdb8ed8eed2
Enesis141215 <enesis141215@gmail.com>
2018-03-30 11:09:41 -0600

    Se añadio las clases para la conversion de Celcius a Kelvin

---
+++ CalculosConversiones/CalculosConversiones/InterfazGradosK_C.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculosConversiones
+{
+    class InterfazGradosK_C
+    {
+        private double GradosKelvin;
+        private double Convertir;
+
+
+        public InterfazGradosK_C()
+        {
+            GradosKelvin = 0;
+            Convertir = 0;
+        }
+
+        public void PedirGradoskelvin()
+        {
+            Console.WriteLine("Introduce la temperatura en grados Kelvin que deceas convertir a Celcius:");
+            double Celcius = double.Parse(Console.ReadLine());
+            Console.ReadKey();
+        }
+        public double SetGradosKelvin()
+        {
+            return GradosKelvin;
+        }
+
+        public double SetConvertir()
+        {
+            return Convertir;
+        }
+        public double GetConvetir()
+        {
+            return Convertir;
+        }
+
+
+        public void ImprimirConversion(double Convertir)
+        {
+            Console.WriteLine("Los grados en Celcius son : " + Convertir + "C");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CalculosConversiones/CalculosConversiones/Kelvin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculosConversiones
+{
+    class Kelvin
+    {
+        private double GradosKelvin;
+        private double Convertir;
+
+
+        public Kelvin()
+        {
+            GradosKelvin = 0;
+            Convertir = 0;
+        }
+        public double SetGradosKelvin(double GradosKelvin)
+        {
+            return GradosKelvin;
+        }
+        public double GetGradosKelvin()
+        {
+            return GradosKelvin;
+        }
+        public double SetConvertir(double Convertir)
+        {
+            return Convertir;
+        }
+        public double GetConvertir()
+        {
+            return Convertir;
+        }
+        //Grados Kelvin a Celcius 
+        public void CovertirKelvinaCelcius()
+        {
+            Convertir = GradosKelvin - 273.15;
+        }
+    }
+}
