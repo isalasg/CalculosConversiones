ec8feb137788b225994e6a2bbbeb5ad0573e1726
Luis Moises Vega Aguero <luis_moises1997@hotmail.com>
2018-03-29 23:35:32 -0600

    Se añadio la clase Conversion e InterfazConversion

---
+++ CalculosConversiones/CalculosConversiones/Conversion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculosConversiones
+{
+    class Conversion
+    {
+        private double Kilo;
+        private double Libra;
+
+
+        public Conversion()
+        {
+            Kilo = 0;
+            Libra = 0;
+
+        }
+
+        public void SetKilo(double Kilo)
+        {
+            this.Kilo = Kilo;
+        }
+
+        public double GetKilo()
+        {
+            return Kilo;
+        }
+
+
+        public void SetLibra(double Libra)
+        {
+            this.Libra = Libra;
+        }
+
+        public double GetLibra()
+        {
+            return Libra;
+        }
+
+        public void CalcularLK()
+        {
+            Kilo = Libra * 0.453592;
+        }
+        public void CalcularKL()
+        {
+            Libra = Kilo * 2.20462;
+        }
+
+
+    }
+}
---
+++ CalculosConversiones/CalculosConversiones/InterfazConversion.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculosConversiones
+{
+    class InterfazConversion
+    {
+        private double Kilo;
+        private double Libra;
+
+        public InterfazConversion()
+        {
+            Kilo = 0;
+            Libra = 0;
+
+        }
+
+        public void PedirKilo()
+        {
+            Console.Write("Escribe cuantos kilos quieres convertir a Libras");
+            Kilo = double.Parse(Console.ReadLine());
+        }
+
+        public void PedirLibra()
+        {
+            Console.Write("Escribe cuantos Libras quieres convertir a Kilos");
+            Kilo = double.Parse(Console.ReadLine());
+        }
+
+        public void SetKilo(double Kilo)
+        {
+            this.Kilo = Kilo;
+        }
+
+        public double GetKilo()
+        {
+            return Kilo;
+        }
+
+
+        public void SetLibra(double Libra)
+        {
+            this.Libra = Libra;
+        }
+
+        public double GetLibra()
+        {
+            return Libra;
+        }
+
+        public void ImprimirKilo(double Kilo)
+        {
+            Console.WriteLine("El resultado es: " + Kilo);
+        }
+
+        public void ImprimirLibra(double Libra)
+        {
+            Console.WriteLine("El resultado es: " + Libra);
+        }
+
+
+
+
+
+        
+
+    }
+}
