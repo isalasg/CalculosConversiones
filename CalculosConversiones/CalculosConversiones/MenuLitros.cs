612f5dceb2664d05745b839070a89363bbf3c002
AaronSernaJuarez <37466661+AaronSernaJuarez@users.noreply.github.com>
2018-03-29 23:12:50 -0600

    Add files via upload

---
+++ CalculosConversiones/CalculosConversiones/MenuLitros.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Litros_Galones
+{
+    public partial class MenuLitros : Form
+    {
+        double Vlitros, Vgalones, result1, result2;
+        public MenuLitros()
+        {
+            InitializeComponent();
+        }
+
+        private void BTNcalcular_Click(object sender, EventArgs e)
+        {
+            Vlitros = double.Parse(TXTlitros.Text);
+            result1 = Vlitros * 0.26417205 / 1;
+            TXTresultado1.Text = result1.ToString();
+
+            Vgalones = double.Parse(TXTgalones.Text);
+            result2 = Vgalones * 3.78541178 / 1;
+            TXTresultado2.Text = result2.ToString();
+
+        }
+    }
+}
